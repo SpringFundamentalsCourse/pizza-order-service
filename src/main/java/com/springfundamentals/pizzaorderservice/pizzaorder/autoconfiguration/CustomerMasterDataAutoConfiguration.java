9bc4e974190405da16c502a373a13243c3ea6e18
scje <jerome.schaub@gmx.ch>
2022-11-15 10:51:10 +0100

    Add solution for hands-on 1.1

---
+++ src/main/java/com/springfundamentals/pizzaorderservice/pizzaorder/autoconfiguration/CustomerMasterDataAutoConfiguration.java
@@ -0,0 +1,21 @@
+package com.springfundamentals.pizzaorderservice.pizzaorder.autoconfiguration;
+
+import api.CustomerMasterDataClient;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+@ConditionalOnMissingBean(CustomerMasterDataClient.class)
+@ConditionalOnProperty(prefix = "custom.customerMasterData", name = "enabled", havingValue = "true")
+public class CustomerMasterDataAutoConfiguration {
+
+    @Bean
+    public CustomerMasterDataClient registerBean(){
+        System.out.println("Creating Customer Master Client from Auto Configuration");
+        return new CustomerMasterDataClient();
+    }
+}
---
+++ src/main/java/com/springfundamentals/pizzaorderservice/pizzaorder/configuration/ApiConfiguration.java
@@ -0,0 +1,15 @@
+package com.springfundamentals.pizzaorderservice.pizzaorder.configuration;
+
+import api.CustomerMasterDataClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiConfiguration {
+
+    /*@Bean
+    public CustomerMasterDataClient customerMasterDataClient() {
+        System.out.println("Creating from Application Configuration");
+        return new CustomerMasterDataClient();
+    }*/
+}
